b9bb323477b30f7d4ddbd4b9625ccecc45823d4f
chawlid <chawlidyoussef@gmail.com>
2025-05-20 19:16:23 +0100

    push

--- main.py
+++ main.py
@@ -29,6 +29,3 @@ async def send():
     print("Send push notification")
     
     return {"message": "Push notification sent"}
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
