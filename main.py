54fb405a5f5f16e9b6f6e125cf4aff0231210b8b
chawlid <chawlidyoussef@gmail.com>
2025-05-24 22:02:47 +0100

    push

--- main.py
+++ main.py
@@ -14,8 +14,7 @@ class Item(BaseModel):
 @app.post("/send")
 async def create_token(item: Item):
     print("Token created")
-    print(item.token)
-    print(item.time)
+    #print(item.token)
     sp.send_data(item.token, item.time)
     return {"token":  item}
 
@@ -27,6 +26,7 @@ async def root():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="192.168.1.48", port=8000, reload=True)
+    uvicorn.run("main:app",port=8000, reload=True)
 #ngrok config add-authtoken 2r8bFq6GpUfmyrs3PIu13bCYU8e_2oQHWK2hWMYP22TazUnax
-#ngrok http http://127.0.0.1:8000
+#ngrok http http://127.0.0.1:8000/
+#https://technodevwebsite.blogspot.com/p/https77da-105-155-176-99ngrok-freeapp.html
